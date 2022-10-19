fa341bc2307ce82484eaad324a778f9971b9c68c
Diego Neves <pdcassiano@gmail.com>
2022-10-19 08:58:25 -0300

    feat: add flatlist buscando dos repositories

--- App.js
+++ App.js
@@ -1,14 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 import Card from './src/components/card/card.component'
+import axios from 'axios'
+import { useState } from 'react'
 
 export default function App() {
+  const [userName, setUserName] = useState('pdiegoneves')
+
+  const [name, setName] = useState('')
+  const [avatar_url, setAvatar_url] = useState('')
+  const [html_url, setHtml_url] = useState('')
+  const [bio, setBio] = useState('')
+  const [blog, setBlog] = useState('')
+  const [email, setEmail] = useState('')
+  const [followers, setFollowers] = useState('')
+  const [following, setFollowing] = useState('')
+  const [location, setLocation] = useState('')
+  const [respos_url, setRepos_url] = useState('')
+  const [repositories, setRepositories] = useState([])
+
+
+  getRemoteUser(userName)
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Card />
+      <Card
+        name={name}
+        avatar_url={avatar_url}
+        html_url={html_url}
+        bio={bio}
+        blog={blog}
+        email={email}
+        followers={followers}
+        following={following}
+        location={location}
+        repositories={repositories}
+      />
     </View>
-  );
+  )
+
+  function getRemoteUser(userName) {
+    axios.get(`https://api.github.com/users/${userName}`).then((res) => {
+      setName(res.data.name)
+      setAvatar_url(res.data.avatar_url)
+      setHtml_url(res.data.html_url)
+      setBio(res.data.bio)
+      setBlog(res.data.blog)
+      setEmail(res.data.email)
+      setFollowers(res.data.followers)
+      setFollowing(res.data.following)
+      setLocation(res.data.location)
+      setRepos_url(`${res.data.repos_url}?perpage=10`)
+    })
+    
+    axios.get(respos_url)
+      .then(res => {
+        setRepositories([
+          { id: res.data[0].id, name: res.data[0].name },
+          { id: res.data[1].id, name: res.data[1].name },
+          { id: res.data[2].id, name: res.data[3].name }
+        ])
+      })
+    
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +73,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,68 +7,30 @@ import {
   TextInput,
   SafeAreaView,
 } from 'react-native'
-import axios from 'axios'
-import { useState } from 'react'
 import Avatar from '../avatar/avatar.component'
 
-export default function Card() {
-  const [userName, setUserName] = useState('pdiegoneves')
+export default function Card(props) {
 
-  const [avatar_url, setAvatar_url] = useState('')
-  const [bio, setBio] = useState('')
-  const [blog, setBlog] = useState('')
-  const [email, setEmail] = useState('')
-  const [followers, setFollowers] = useState('')
-  const [following, setFollowing] = useState('')
-  const [html_url, setHtml_url] = useState('')
-  const [id, setId] = useState('')
-  const [location, setLocation] = useState('')
-  const [name, setName] = useState('')
-  const [repos_url, setRepos_url] = useState('')
-  const [repos, setRepos] = useState([{name: '', description: ''}])
-
-  const baseURL = `https://api.github.com/users/${userName}`
-  axios.get(baseURL).then((res) => {
-    setAvatar_url(res.data.avatar_url)
-    setBio(res.data.bio)
-    setBlog(res.data.blog)
-    setEmail(res.data.email)
-    setFollowers(res.data.followers)
-    setFollowing(res.data.following)
-    setHtml_url(res.data.html_url)
-    setId(res.data.id)
-    setLocation(res.data.location)
-    setName(res.data.name)
-    setRepos_url(res.data.repos_url + '?per_page=10')
-    // axios.get(repos_url).then(res => {
-    //   res.data.forEach(repo => {
-    //     setRepos([...repos, { name: repo.name, description: repo.description }])
-    //     console.log(repo)
-    //   }
-
-    //   )
-    // })
-  })
   return (
     <View style={styles.card}>
       <ScrollView>
-        <Avatar uri={avatar_url} />
+        <Avatar uri={props.avatar_url} />
         <View style={styles.info}>
-          <Text style={styles.name}>{name}</Text>
-          <Text style={styles.name}>{html_url}</Text>
+          <Text style={styles.name}>{props.name}</Text>
+          <Text style={styles.name}>{props.html_url}</Text>
           <View style={styles.maisInfo}>
-            <Text>Bio: { bio }</Text>
-            <Text>Blog: { blog }</Text>
-            <Text>E-mail: { email }</Text>
-            <Text>Seguidores: {followers} | Seguindo: { following }</Text>
-            <Text>{ location }</Text>
+            <Text>Bio: { props.bio }</Text>
+            <Text>Blog: { props.blog }</Text>
+            <Text>E-mail: { props.email }</Text>
+            <Text>Seguidores: {props.followers} | Seguindo: { props.following }</Text>
+            <Text>{ props.location }</Text>
           </View>
         </View>
-        <FlatList
-          data={repos}
+      </ScrollView>
+      <FlatList styles={ styles.repoList }
+          data={ props.repositories }
           renderItem={ ({item}) => <Repository data={item} />}
         />
-      </ScrollView>
     </View>
   )
 }
@@ -110,14 +72,21 @@ const styles = StyleSheet.create({
   maisInfo: {
     alignItems: 'flex-start',
     marginTop: 15,
+  },
+  repoList: {
+    margin: 20,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'left'
   }
 })
 
 function Repository(props) {
   return (
     <View>
-      <Text>{props.name}</Text>
-      <Text>{props.description}</Text>
+      <Text>{props.data.name}</Text>
     </View>
   )
 }
