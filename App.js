3a1f3492de8a70be8a813145b9675d4b7f6e5468
Diego Neves <pdcassiano@gmail.com>
2022-11-22 05:32:09 -0300

    restaurando arquivos corretos

--- App.js
+++ App.js
@@ -3,41 +3,66 @@ import { StyleSheet, Text, TextInput, View } from 'react-native'
 import Card from './src/components/card/card.component'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import { SafeAreaView } from 'react-native-web'
 
 export default function App() {
-  const [cep, setCep] = useState('57000000')
-  const [endereco, setEndereco] = useState({})
+  const [userName, setUserName] = useState('pdiegoneves')
+  const [name, setName] = useState('')
+  const [avatar_url, setAvatar_url] = useState('')
+  const [html_url, setHtml_url] = useState('')
+  const [bio, setBio] = useState('')
+  const [blog, setBlog] = useState('')
+  const [email, setEmail] = useState('')
+  const [followers, setFollowers] = useState('')
+  const [following, setFollowing] = useState('')
+  const [location, setLocation] = useState('')
+  const [respos_url, setRepos_url] = useState('')
+  const [repositories, setRepositories] = useState([])
 
-
+  // getRemoteUser(userName)
   useEffect(() => {
     axios
-      .get(`https://viacep.com.br/ws/${cep}/json/`)
+      .get(`https://api.github.com/users/${userName}`)
       .then((res) => {
-       setEndereco(res.data)
-        console.log(endereco)
+        setName(res.data.name)
+        setAvatar_url(res.data.avatar_url)
+        setHtml_url(res.data.html_url)
+        setBio(res.data.bio)
+        setBlog(res.data.blog)
+        setEmail(res.data.email)
+        setFollowers(res.data.followers)
+        setFollowing(res.data.following)
+        setLocation(res.data.location)
+        setRepos_url(res.data.repos_url)
       })
-  }, [cep])
+  }, [userName])
 
-  const handleCep = (e) => {
-    setCep(e.nativeEvent.text)
+  useEffect(() => {
+    axios.get(respos_url).then(res => setRepositories(res.data))
+  }, [respos_url])
+
+  const handleUserName = (e) => {
+    setUserName(e.nativeEvent.text)
   }
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text style={styles.title}>Buscar CEP</Text>
       <TextInput
         style={styles.input}
-        placeholder={cep}
-        onEndEditing={handleCep}
+        placeholder={userName}
+        onEndEditing={handleUserName}
       />
       <Card
-        bairro={endereco.bairro}
-        complemento={endereco.complemento}
-        logradouro={endereco.logradouro}
-        cidade={endereco.localidade}
-        uf={endereco.uf}
+        name={name}
+        avatar_url={avatar_url}
+        html_url={html_url}
+        bio={bio}
+        blog={blog}
+        email={email}
+        followers={followers}
+        following={following}
+        location={location}
+        repositories={repositories}
       />
     </View>
   )
@@ -51,16 +76,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   input: {
+    marginTop: 30,
     height: 40,
     width: '70%',
     borderColor: 'gray',
     backgroundColor: '#fff',
-    borderRadius: 15,
-    padding: 10
   },
-  title: {
-    fontSize: 25,
-    fontWeight: 'bold',
-
-  }
 })
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,20 +7,31 @@ import {
   TextInput,
   SafeAreaView,
 } from 'react-native'
+import Avatar from '../avatar/avatar.component'
 
 export default function Card(props) {
 
   return (
     <View style={styles.card}>
       <ScrollView>
+        <Avatar uri={props.avatar_url} />
         <View style={styles.info}>
-          <Text style={styles.texto}>Logradouro:</Text><Text> {props.logradouro}</Text>
-          <Text style={styles.texto}>Complemento:</Text><Text> {props.complemento}</Text>
-          <Text style={styles.texto}>Bairro:</Text><Text> {props.bairro}</Text>
-          <Text style={styles.texto}>Cidade:</Text><Text>{props.cidade}</Text>
-          <Text style={styles.texto}>UF:</Text><Text> {props.uf}</Text>
+          <Text style={styles.name}>{props.name}</Text>
+          <Text style={styles.name}>{props.html_url}</Text>
+          <View style={styles.maisInfo}>
+            <Text>Bio: { props.bio }</Text>
+            <Text>Blog: { props.blog }</Text>
+            <Text>E-mail: { props.email }</Text>
+            <Text>Seguidores: {props.followers} | Seguindo: { props.following }</Text>
+            <Text>{ props.location }</Text>
+          </View>
         </View>
       </ScrollView>
+      <FlatList style={ styles.repoList }
+          data={ props.repositories }
+          renderItem={({ item }) => <Repository
+          data={item} />}
+          />
     </View>
   )
 }
@@ -28,17 +39,56 @@ export default function Card(props) {
 const styles = StyleSheet.create({
   card: {
     minWidth: '90%',
-    height: '50%',
+    minHeight: 400,
     backgroundColor: '#ddd',
+    flex: 1,
     alignContent: 'center',
     justifyContent: 'center',
     margin: 25,
     borderRadius: 50,
-    paddingTop: 20,
-    paddingLeft: 20,
   },
-  texto: {
-    fontSize: 18,
-    fontWeight: 'bold'
+  input: {
+    height: 40,
+    margin: 30,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 15,
+    backgroundColor: '#fff',
+  },
+  areaInput: {
+    margin: 20,
+    borderRadius: 15,
+    backgroundColor: '#999',
+  },
+  info: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  name: {
+    color: '#333',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  maisInfo: {
+    alignItems: 'flex-start',
+    marginTop: 15,
+  },
+  repoList: {
+    backgroundColor: '#333333',
+    paddingLeft: 40,
+    maxHeight: 200
+  },
+  repo: {
+    color: '#fff',
   }
 })
+
+function Repository(props) {
+  return (
+    <View>
+      <Text style={styles.repo}>-> {props.data.name}</Text>
+    </View>
+  )
+}
